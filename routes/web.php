4dd0c2e28379215a806be1f1a29a51b2bc21096b
Arbahud Rio Daroyni <roydotendless@gmail.com>
2024-08-01 22:14:02 +0700

    create login feature

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Login
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'authenticate'])->name('auth.authenticate');
+
+// Register
+Route::get('/register', [AuthController::class, 'register'])->name('auth.register');
+Route::post('/register', [AuthController::class, 'store'])->name('auth.store');
+
+// Logout
+Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
